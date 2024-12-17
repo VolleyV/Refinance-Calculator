ca799b042a0a403c053dbec995c762c3485b62db
kizeno309 <aingpreechaya@gmail.com>
2024-12-17 10:50:58 +0700

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center py-4">
+          <div className="text-white text-lg font-bold">
+            คำนวณรีไฟแนนซ์ผ่อนบ้าน
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
