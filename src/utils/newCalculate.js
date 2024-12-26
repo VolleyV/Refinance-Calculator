3216ef8926b26ad86e6e37e3e74900c5d5b7403f
VolleyV <nopphoom@gmail.com>
2024-12-26 09:15:28 +0700

    pree-merge

---
+++ src/utils/newCalculate.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+export const newcalculate = () => {
+  return (
+    <div>
+        <input value={"principal"}></input>
+        <input value={"rate"}></input>
+        <input value={"years"}></input>
+    </div>
+  )
+}
