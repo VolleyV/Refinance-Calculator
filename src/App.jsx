045b3d3032494944b2e347995f0ca3a25c424c74
VolleyV <98677220+VolleyV@users.noreply.github.com>
2025-02-05 17:59:05 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -226,11 +226,11 @@ function App() {
   const [compareData, setCompareData] = useState([]);
   const saveToTable = async (advanceSummary) => {
     // บันทึกข้อมูลพร้อม timestamp ลงใน compareData
-    setCompareData((prev) => [...prev, dataWithTimestamp]
     try {
           const dataWithTimestamp = {
       ...advanceSummary,
       timeStamp: Date.now(), // เพิ่ม timestamp ปัจจุบัน
+                setCompareData((prev) => [...prev, dataWithTimestamp]
     };
 
       // Add a unique ID based on timestamp
